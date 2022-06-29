910179bbb18674e34bed4df170c48c0181157fa1
Ksenia Vislova <vislova.fortech@gmail.com>
2022-06-29 16:12:15 +0300

    feat: add functionality for updating tasks in db

--- src/Services/TodoService.js
+++ src/Services/TodoService.js
@@ -10,6 +10,14 @@ class TodoService {
     const todos = await Todo.find();
     return todos;
   }
+
+  static async update(post) {
+    if (!post._id) throw new Error("Id doesn't exist");
+    const updateTodo = await Todo.findByIdAndUpdate(post._id, post, {
+      new: true,
+    });
+    return updateTodo;
+  }
 }
 
 export default TodoService;
--- src/routers/router.js
+++ src/routers/router.js
@@ -5,5 +5,6 @@ const router = new Router();
 
 router.post("/todos", TodoController.add);
 router.get("/todos", TodoController.getAll);
+router.put("/todos/:id", TodoController.update);
 
 export default router;
