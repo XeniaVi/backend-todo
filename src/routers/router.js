433c54900fe700de8c35c05e37976b999443f0de
Ksenia Vislova <vislova.fortech@gmail.com>
2022-06-29 16:41:17 +0300

    fix: fix rout

--- src/routers/router.js
+++ src/routers/router.js
@@ -5,6 +5,6 @@ const router = new Router();
 
 router.post("/todos", TodoController.add);
 router.get("/todos", TodoController.getAll);
-router.put("/todos/:id", TodoController.update);
+router.put("/todos", TodoController.update);
 
 export default router;
