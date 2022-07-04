e5cfdcae6569a22339f7ee5f63f69fe52a52ec04
Ksenia Vislova <vislova.fortech@gmail.com>
2022-07-04 16:41:39 +0300

    refactor: resolve comments after review

--- src/validators/todoValidators.js
+++ src/validators/todoValidators.js
@@ -1,23 +1,27 @@
 export const validateNewTodo = (req, res, next) => {
-  let post = req.body;
-  const length = Object.keys(post).length;
-  if (
-    length !== 2 ||
-    !(post.value || post.value.length) ||
-    typeof post.completed !== "boolean"
-  )
+  const { value, completed } = req.body;
+
+  if (!value || typeof value !== "string" || typeof completed !== "boolean") {
     throw new Error("Wrong data!");
+  }
+
   next();
 };
 
 export const validateUpdateTodo = (req, res, next) => {
-  const post = req.body;
-  const length = Object.keys(post).length;
+  const { value, completed } = req.body;
 
-  if (
-    (length !== 1 && !(post.value || post.value.length)) ||
-    (length !== 1 && typeof post.completed !== "boolean")
-  )
+  if (value === undefined && completed === undefined) {
     throw new Error("Wrong data!");
+  }
+
+  if (value && (typeof value !== "string" || !value.length)) {
+    throw new Error("Wrong data!");
+  }
+
+  if (completed && typeof completed !== "boolean") {
+    throw new Error("Wrong data!");
+  }
+
   next();
 };
