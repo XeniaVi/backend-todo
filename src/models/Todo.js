d2a8485f9e5312ce0ff24d6d88d07f551e17c9ce
Ksenia Vislova <vislova.fortech@gmail.com>
2022-07-07 15:11:11 +0300

    feat: rename timestamp to createdAt

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Todo = new mongoose.Schema({
   value: { type: String, required: true },
   completed: { type: Boolean, required: true },
-  timestamp: { type: Number, required: true },
+  createdAt: { type: Number, required: true },
 });
 
 export default mongoose.model("Todo", Todo);
